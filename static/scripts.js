a7d02558db7a09414f7435ec08261135caea6924
kyych <m.biernat43@gmail.com>
2020-02-22 15:45:17 +0100

    change name of button for saving the note

--- static/scripts.js
+++ static/scripts.js
@@ -1,12 +1,13 @@
 function addNote(){
-    var btn = document.createElement("DIV");
-    btn.className = "inputDiv";
+    var inputBox = document.createElement("DIV");
+    inputBox.className = "inputDiv";
     // btn.innerHTML = "CLICK ME";
     var content = document.createElement("INPUT")
     var button = document.createElement("BUTTON")
+    button.className= "btn";
     button.innerHTML = "CLICK ME";
     // document.body.appendChild(btn);
-    btn.appendChild(content);
-    btn.appendChild(button);
-    document.body.appendChild(btn);
+    inputBox.appendChild(content);
+    inputBox.appendChild(button);
+    document.body.appendChild(inputBox);
 }
